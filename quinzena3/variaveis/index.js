6c743c256f01b126d8805d4a75d180553056795a
Victoria Veloso <victoriaveferreira@gmail.com>
2020-11-17 23:54:04 -0300

    Exercício 1 JS

---
+++ quinzena3/variaveis/index.js
@@ -0,0 +1,58 @@
+// let name ="";
+// let age = "";
+
+// console.log(typeof(name));
+// console.log(typeof(age));
+
+// a resposta foi undefined, porque não foi definido nenhum valor para as variaveis 
+
+// //1
+// let nome = prompt("Qual seu nome?");
+// let idade = prompt("Quantos anos você tem?");
+// console.log(`Olá ${nome}, você tem ${idade} anos`);
+
+//2
+// let cor = prompt("Qual sua cor preferida?");
+// console.log(`1. Qual sua cor preferida?`);
+// console.log(`Resposta: ${cor}`);
+
+// let numero = prompt("Qual seu número da sorte?");
+// console.log(`2. Qual seu número da sorte?`);
+// console.log(`Resposta: ${numero}`);
+
+// let comida = prompt("Qual seu prato preferido?");
+// console.log(`3. Qual seu prato preferido?`);
+// console.log(`Resposta: ${comida}`);
+
+// let bebida = prompt("Qual sua bebida preferida?");
+// console.log(`4. Qual sua bebida preferida?`);
+// console.log(`Resposta: ${bebida}`);
+
+// let jogo = prompt("Qual seu jogo preferido?");
+// console.log(`5. Qual seu jogo preferido?`);
+// console.log(`Resposta: ${jogo}`);
+
+//3
+// let food = ["pizza", "carne", "sushi", "salada", "lasanha"];
+// console.log(food);
+// console.log(`Essas são as minhas comidas preferidas:`)
+// console.log(food[0]);
+// console.log(food[1]);
+// console.log(food[2]);
+// console.log(food[3]);
+// console.log(food[4]);
+// let perguntaFood = prompt("Qual seu prato preferido?");
+// console.log(perguntaFood);
+// console.log(`Essas são as minhas comidas preferidas:`)
+// console.log(food[0]);
+// console.log(perguntaFood);
+// console.log(food[2]);
+// console.log(food[3]);
+// console.log(food[4]);
+
+//4
+let questions = ["Você gosta de natal?", "Você gosta de azul?", "Você dormiu bem hoje?"];
+let bool = [false, true, false];
+console.log(`"${questions[0]}" ${bool[0]}`);
+console.log(`"${questions[1]}" ${bool[1]}`);
+console.log(`"${questions[2]}" ${bool[2]}`)
